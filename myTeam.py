05601d4e77e13a644fbcc282625a94db0b6fa705
ARNAUSOLANSIVAZQUEZ <114942175+ARNAUSOLANSIVAZQUEZ@users.noreply.github.com>
2023-12-04 21:32:01 +0100

    Add files via upload
    Improved agents

--- myTeam.py
+++ myTeam.py
@@ -136,41 +136,162 @@ class ReflexCaptureAgent(CaptureAgent):
 
 class StallReflexAgent(ReflexCaptureAgent):
     def get_features(self, game_state, action):
+        # Initializing features counter
         features = util.Counter()
-        successor = self.get_successor(game_state, action)
-        food_list = self.get_food(successor).as_list()
-        my_food = self.get_food_you_are_defending(game_state).as_list()
 
+        # Retrieving successor state
+        successor = self.get_successor(game_state, action)
+        # Retrieving own/enemy food list for successor state
+        enemy_food = self.get_food(successor).as_list()
+        my_food = self.get_food_you_are_defending(successor).as_list()
+
+        # Retrieving own/enemy food list for current state
+        cur_enemy_food = self.get_food(game_state).as_list()
+        cur_my_food = self.get_food(game_state).as_list()
+
+        # Retrieving own state and position for successor state
         my_state = successor.get_agent_state(self.index)
         my_pos = successor.get_agent_state(self.index).get_position()
+        # Retrieving enemy states for successor state
         enemies = [successor.get_agent_state(i) for i in self.get_opponents(successor)]
         invaders = [a for a in enemies if a.is_pacman and a.get_position() is not None]
-        features['num_invaders'] = len(invaders)
-        features['successor_score'] = -len(food_list)  # self.getScore(successor)
-        features['on_defense'] = 0
-        if len(food_list) > 0:
-            min_distance = min([self.get_maze_distance(my_pos, food) for food in food_list])
-            enemy_positions = []
-            for a in enemies:
-                position = a.get_position()
-                if position is not None:
-                    enemy_positions.append(position)
-            if len(enemy_positions) > 0:
-                min_enemy_distance = min([self.get_maze_distance(my_pos, enemy_pos) for enemy_pos in enemy_positions])
-            else:
-                min_enemy_distance = 1
-            features['objective_distance'] = min_distance / min_enemy_distance
-        if len(food_list) < 20:
-            if not(my_state.is_pacman): features['on_defense'] = 1
-            if len(invaders) > 0:
-                min_distance = min([self.get_maze_distance(my_pos, a.get_position()) for a in invaders])
-                features['objective_distance'] = min_distance
-            else:
-                min_distance = min([self.get_maze_distance(my_pos, food) for food in my_food])
-                features['objective_distance'] = min_distance
 
+        # Retrieving score for current game state
+        score = self.get_score(game_state)
+
+        # If we are not winning by a margin of 5 toggle offensive behaviour
+        if score <= 5:
+            # Defining winning margin based on current food
+            win_margin = len(my_food) - len(enemy_food)
+            print(win_margin)
+            # If we are not 5 foods ahead toggle offensive behaviour // If we are not going to lose
+            if win_margin < 5 and len(my_food) != 2:
+                print('OFFENSE')
+                # Toggle offense
+                features['on_defense'] = 0
+                features['successor_score'] = len(enemy_food)
+                # Agent is not fearful until it sees enemy ghosts
+                features['fear'] = 0
+                # Nullify defensive feature
+                features['num_invaders'] = 0
+                # Check if there is remaining enemy food
+                if len(enemy_food) > 0:
+                    # Retrieving minimum distance to enemy food
+                    min_distance = min([self.get_maze_distance(my_pos, food) for food in enemy_food])
+                    # Compute visible enemy ghosts distances
+                    enemy_positions = []
+                    for a in enemies:
+                        if not a.is_pacman:
+                            position = a.get_position()
+                            if position is not None:
+                                enemy_positions.append(position)
+                    # If agent is on enemy field make it fearful of enemies
+                    if my_state.is_pacman:
+                        # If any enemy is visible compute minimum distance between agent and enemy
+                        if len(enemy_positions) > 0:
+                            min_enemy_distance = min([self.get_maze_distance(my_pos, enemy_pos) for enemy_pos in enemy_positions])
+                            features['fear'] = 1/min_enemy_distance
+                    # Define objective distance as a ratio between food distance and enemy distance
+                    features['objective_distance'] = min_distance
+            # Else toggle defensive behaviour
+            else:
+                print('DEFENSE')
+                # Toggle defense
+                features['on_defense'] = 1
+                features['num_invaders'] = len(invaders)
+                # Nullify offensive feature
+                features['fear'] = 0
+                features['successor_score'] = 0
+                # Compute visible enemy ghosts distances
+                enemy_positions = []
+                for a in enemies:
+                    if not a.is_pacman:
+                        position = a.get_position()
+                        if position is not None:
+                            enemy_positions.append(position)
+                # If agent is on enemy field make it fearful of enemies (returning)
+                if my_state.is_pacman:
+                    # If any enemy is visible compute minimum distance between agent and enemy
+                    if len(enemy_positions) > 0:
+                        min_enemy_distance = min([self.get_maze_distance(my_pos, enemy_pos) for enemy_pos in enemy_positions])
+                        features['fear'] = 1 / min_enemy_distance
+                    # If there are invaders compute minimum distance to invader
+                    if len(invaders) > 0:
+                        min_distance = min([self.get_maze_distance(my_pos, a.get_position()) for a in invaders])
+                        # Define objective distance as minimum distance from agent to invader
+                        features['objective_distance'] = min_distance
+                    # Else compute distance to own food
+                    else:
+                        min_distance = min([self.get_maze_distance(my_pos, food) for food in my_food])
+                        # Define objective distance as minimum distance from agent to own food
+                        features['objective_distance'] = min_distance
+        # If we are winning with a margin of 5 toggle defense
+        else:
+            print('WINNING')
+            features['on_defense'] = 1
+            features['successor_score'] = 0
+            features['fear'] = 0
+            features['num_invaders'] = len(invaders)
+            # If there are invaders compute minimum distance to invader
+            if len(invaders) > 0:
+                    min_distance = min([self.get_maze_distance(my_pos, a.get_position()) for a in invaders])
+                    # Define objective distance as minimum distance from agent to invader
+                    features['objective_distance'] = min_distance
+            # Else compute distance to own food
+            else:
+                    min_distance = min([self.get_maze_distance(my_pos, food) for food in my_food])
+                    # Define objective distance as minimum distance from agent to own food
+                    features['objective_distance'] = min_distance
+        # Controlling passive behaviour based on if the agent is on the offense or defense
+        if features['on_defense'] == 1:
+            if action == Directions.STOP:
+                features['stop'] = 0
+        elif features['on_defense'] == 0:
+            if action == Directions.STOP:
+                features['stop'] = 1
         return features
 
     def get_weights(self, game_state, action):
-        return {'successor_score': 100, 'on_defense': 100,
-                'objective_distance': -10, 'num_invaders': -1000}
+        return {'successor_score': -100, 'on_defense': 100,
+                'objective_distance': -1, 'num_invaders': -1000,
+                'stop': -100, 'fear': -10}
+
+class DefenseReflexAgent(ReflexCaptureAgent):
+    def get_features(self, game_state, action):
+        # Initializing features counter
+        features = util.Counter()
+        # Retrieving successor state
+        successor = self.get_successor(game_state, action)
+        my_food = self.get_food_you_are_defending(successor).as_list()
+        # Retrieving own state and position for successor state
+        my_state = successor.get_agent_state(self.index)
+        my_pos = successor.get_agent_state(self.index).get_position()
+        # Retrieving enemy states for successor state
+        enemies = [successor.get_agent_state(i) for i in self.get_opponents(successor)]
+        invaders = [a for a in enemies if a.is_pacman and a.get_position() is not None]
+        # Initializing features for number of invaders and score of successor state
+        features['num_invaders'] = len(invaders)
+        # Initializing current behaviour (offense/defense)
+        features['on_defense'] = 1
+        # If there are invaders compute minimum distance to invader
+        if len(invaders) > 0:
+            min_distance = min([self.get_maze_distance(my_pos, a.get_position()) for a in invaders])
+            # Define objective distance as minimum distance from agent to invader
+            features['objective_distance'] = min_distance
+        # Else compute distance to own food
+        else:
+            min_distance = min([self.get_maze_distance(my_pos, food) for food in my_food])
+            # Define objective distance as minimum distance from agent to own food
+            features['objective_distance'] = min_distance
+        # Controlling passive behaviour based on if the agent is on the offense or defense
+        if features['on_defense'] == 1:
+            if action == Directions.STOP:
+                features['stop'] = 0
+        elif features['on_defense'] == 0:
+            if action == Directions.STOP:
+                features['stop'] = 1
+        return features
+
+    def get_weights(self, game_state, action):
+        return {'on_defense': 100, 'objective_distance': -1,
+                'num_invaders': -1000, 'stop': -100}
