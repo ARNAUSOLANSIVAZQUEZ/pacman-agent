58371cadfa15921a8079b79a253946cb882a67ee
ARNAUSOLANSIVAZQUEZ <114942175+ARNAUSOLANSIVAZQUEZ@users.noreply.github.com>
2023-12-07 22:58:19 +0100

    Added and improved agents

--- myTeam.py
+++ myTeam.py
@@ -33,7 +33,7 @@ from util import nearestPoint
 #################
 
 def create_team(first_index, second_index, is_red,
-                first='StallReflexAgent', second='StallReflexAgent', num_training=0):
+                first='ToxicReflexAgent', second='ProtectionReflexAgent', num_training=0):
     """
     This function should return a list of two agents that will form the
     team, initialized using firstIndex and secondIndex as their agent
@@ -134,24 +134,26 @@ class ReflexCaptureAgent(CaptureAgent):
         """
         return {'successor_score': 1.0}
 
-class StallReflexAgent(ReflexCaptureAgent):
+
+class ToxicReflexAgent(ReflexCaptureAgent):
+    # Defining adaptive agent that attacks keeping a victory margin to stay ahead. If we are close to lose
+    # or we already surpass the margin it falls back to defense. Mirrors defensive behaviour of Protect agent
+    # except it doesn't care about the power pill.
     def get_features(self, game_state, action):
         # Initializing features counter
         features = util.Counter()
 
         # Retrieving successor state
         successor = self.get_successor(game_state, action)
+
         # Retrieving own/enemy food list for successor state
         enemy_food = self.get_food(successor).as_list()
         my_food = self.get_food_you_are_defending(successor).as_list()
 
-        # Retrieving own/enemy food list for current state
-        cur_enemy_food = self.get_food(game_state).as_list()
-        cur_my_food = self.get_food(game_state).as_list()
-
         # Retrieving own state and position for successor state
         my_state = successor.get_agent_state(self.index)
-        my_pos = successor.get_agent_state(self.index).get_position()
+        my_pos = my_state.get_position()
+
         # Retrieving enemy states for successor state
         enemies = [successor.get_agent_state(i) for i in self.get_opponents(successor)]
         invaders = [a for a in enemies if a.is_pacman and a.get_position() is not None]
@@ -161,10 +163,11 @@ class StallReflexAgent(ReflexCaptureAgent):
 
         # If we are not winning by a margin of 5 toggle offensive behaviour
         if score <= 5:
-            # Defining winning margin based on current food
+            # Defining winning margin based on next food lists
             win_margin = len(my_food) - len(enemy_food)
-            # If we are not 5 foods ahead toggle offensive behaviour // If we are not going to lose
-            if win_margin < 5 and len(my_food) != 2:
+            # If we are not 5 foods ahead toggle offensive behaviour
+            # and If we are not close to losing (only 2 or less food remaining on our side)
+            if win_margin < 5 and len(my_food) > 2:
                 # Toggle offense
                 features['on_defense'] = 0
                 features['successor_score'] = len(enemy_food)
@@ -175,22 +178,25 @@ class StallReflexAgent(ReflexCaptureAgent):
                 # Check if there is remaining enemy food
                 if len(enemy_food) > 0:
                     # Retrieving minimum distance to enemy food
-                    min_distance = min([self.get_maze_distance(my_pos, food) for food in enemy_food])
-                    # Compute visible enemy ghosts distances
-                    enemy_positions = []
-                    for a in enemies:
-                        if not a.is_pacman:
-                            position = a.get_position()
-                            if position is not None:
-                                enemy_positions.append(position)
+                    food_distances = [self.get_maze_distance(my_pos, food) for food in enemy_food]
+                    min_food_distance = min(food_distances)
+                    # Define objective distance as distance from closer enemy food
+                    features['objective_distance'] = min_food_distance
                     # If agent is on enemy field make it fearful of enemies
                     if my_state.is_pacman:
+                        # Compute visible non-scared enemy ghosts distances
+                        enemy_positions = []
+                        for a in enemies:
+                            if not(a.is_pacman) and a.scared_timer == 0:
+                                position = a.get_position()
+                                if position is not None:
+                                    enemy_positions.append(position)
                         # If any enemy is visible compute minimum distance between agent and enemy
                         if len(enemy_positions) > 0:
-                            min_enemy_distance = min([self.get_maze_distance(my_pos, enemy_pos) for enemy_pos in enemy_positions])
+                            enemy_distances = [self.get_maze_distance(my_pos, enemy_pos) for enemy_pos in enemy_positions]
+                            min_enemy_distance = min(enemy_distances)
+                            # Define fear proportionally inverse to closer ghost
                             features['fear'] = 1/min_enemy_distance
-                    # Define objective distance as a ratio between food distance and enemy distance
-                    features['objective_distance'] = min_distance
             # Else toggle defensive behaviour
             else:
                 # Toggle defense
@@ -199,29 +205,33 @@ class StallReflexAgent(ReflexCaptureAgent):
                 # Nullify offensive feature
                 features['fear'] = 0
                 features['successor_score'] = 0
-                # Compute visible enemy ghosts distances
-                enemy_positions = []
-                for a in enemies:
-                    if not a.is_pacman:
-                        position = a.get_position()
-                        if position is not None:
-                            enemy_positions.append(position)
                 # If agent is on enemy field make it fearful of enemies (returning)
                 if my_state.is_pacman:
+                    # Compute visible non-scared enemy ghosts distances
+                    enemy_positions = []
+                    for a in enemies:
+                        if not (a.is_pacman) and a.scared_timer == 0:
+                            position = a.get_position()
+                            if position is not None:
+                                enemy_positions.append(position)
                     # If any enemy is visible compute minimum distance between agent and enemy
                     if len(enemy_positions) > 0:
-                        min_enemy_distance = min([self.get_maze_distance(my_pos, enemy_pos) for enemy_pos in enemy_positions])
+                        enemy_distances = [self.get_maze_distance(my_pos, enemy_pos) for enemy_pos in enemy_positions]
+                        min_enemy_distance = min(enemy_distances)
+                        # Define fear proportionally inverse to closer ghost
                         features['fear'] = 1 / min_enemy_distance
-                    # If there are invaders compute minimum distance to invader
-                    if len(invaders) > 0:
-                        min_distance = min([self.get_maze_distance(my_pos, a.get_position()) for a in invaders])
-                        # Define objective distance as minimum distance from agent to invader
-                        features['objective_distance'] = min_distance
-                    # Else compute distance to own food
-                    else:
-                        min_distance = min([self.get_maze_distance(my_pos, food) for food in my_food])
-                        # Define objective distance as minimum distance from agent to own food
-                        features['objective_distance'] = min_distance
+                # If there are invaders compute minimum distance to invader
+                if len(invaders) > 0:
+                    invader_distances = [self.get_maze_distance(my_pos, a.get_position()) for a in invaders]
+                    min_invader_distance = min(invader_distances)
+                    # Define objective distance as minimum distance from agent to invader
+                    features['objective_distance'] = min_invader_distance
+                # Else compute distance to own food
+                else:
+                    my_food_distances = [self.get_maze_distance(my_pos, food) for food in my_food]
+                    min_my_food_distance = min(my_food_distances)
+                    # Define objective distance as minimum distance from agent to own food
+                    features['objective_distance'] = min_my_food_distance
         # If we are winning with a margin of 5 toggle defense
         else:
             features['on_defense'] = 1
@@ -230,18 +240,22 @@ class StallReflexAgent(ReflexCaptureAgent):
             features['num_invaders'] = len(invaders)
             # If there are invaders compute minimum distance to invader
             if len(invaders) > 0:
-                    min_distance = min([self.get_maze_distance(my_pos, a.get_position()) for a in invaders])
-                    # Define objective distance as minimum distance from agent to invader
-                    features['objective_distance'] = min_distance
+                invader_distances = [self.get_maze_distance(my_pos, a.get_position()) for a in invaders]
+                min_invader_distance = min(invader_distances)
+                # Define objective distance as minimum distance from agent to invader
+                features['objective_distance'] = min_invader_distance
             # Else compute distance to own food
             else:
-                    min_distance = min([self.get_maze_distance(my_pos, food) for food in my_food])
-                    # Define objective distance as minimum distance from agent to own food
-                    features['objective_distance'] = min_distance
+                my_food_distances = [self.get_maze_distance(my_pos, food) for food in my_food]
+                min_my_food_distance = min(my_food_distances)
+                # Define objective distance as minimum distance from agent to own food
+                features['objective_distance'] = min_my_food_distance
         # Controlling passive behaviour based on if the agent is on the offense or defense
+        # Defensive behaviour allows agent to stop on top of ally food
         if features['on_defense'] == 1:
             if action == Directions.STOP:
                 features['stop'] = 0
+        # Aggressive behaviour forces agent to keep moving towards current objective
         elif features['on_defense'] == 0:
             if action == Directions.STOP:
                 features['stop'] = 1
@@ -252,40 +266,54 @@ class StallReflexAgent(ReflexCaptureAgent):
                 'objective_distance': -1, 'num_invaders': -1000,
                 'stop': -100, 'fear': -10}
 
-class DefenseReflexAgent(ReflexCaptureAgent):
+
+class ProtectionReflexAgent(ReflexCaptureAgent):
+    # Defining fully defensive agent to protect our side with priority:
+    # Invader > Power Pill > Own Food
     def get_features(self, game_state, action):
         # Initializing features counter
         features = util.Counter()
+
         # Retrieving successor state
         successor = self.get_successor(game_state, action)
+
+        # Retrieving own food list for successor state
         my_food = self.get_food_you_are_defending(successor).as_list()
+        # Retrieving own power pill for successor state
+        my_power = self.get_capsules_you_are_defending(game_state)
+
         # Retrieving own state and position for successor state
         my_state = successor.get_agent_state(self.index)
-        my_pos = successor.get_agent_state(self.index).get_position()
+        my_pos = my_state.get_position()
+
         # Retrieving enemy states for successor state
         enemies = [successor.get_agent_state(i) for i in self.get_opponents(successor)]
         invaders = [a for a in enemies if a.is_pacman and a.get_position() is not None]
-        # Initializing features for number of invaders and score of successor state
-        features['num_invaders'] = len(invaders)
-        # Initializing current behaviour (offense/defense)
+
         features['on_defense'] = 1
+        if my_state.is_pacman:
+            features['on_defense'] == 0
+        features['num_invaders'] = len(invaders)
+        if action == Directions.STOP:
+            features['stop'] = 0
         # If there are invaders compute minimum distance to invader
         if len(invaders) > 0:
-            min_distance = min([self.get_maze_distance(my_pos, a.get_position()) for a in invaders])
+            invader_distances = [self.get_maze_distance(my_pos, a.get_position()) for a in invaders]
+            min_invader_distance = min(invader_distances)
             # Define objective distance as minimum distance from agent to invader
-            features['objective_distance'] = min_distance
-        # Else compute distance to own food
+            features['objective_distance'] = min_invader_distance
+        # Else if there are own power pills compute minimum distance to power pill
+        elif len(my_power) > 0:
+            my_power_distances = [self.get_maze_distance(my_pos, power) for power in my_power]
+            min_my_power_distance = min(my_power_distances)
+            # Define objective distance as minimum distance from agent to power pill
+            features['objective_distance'] = min_my_power_distance
+        # Else compute distances to own food
         else:
-            min_distance = min([self.get_maze_distance(my_pos, food) for food in my_food])
+            my_food_distances = [self.get_maze_distance(my_pos, food) for food in my_food]
+            min_my_food_distance = min(my_food_distances)
             # Define objective distance as minimum distance from agent to own food
-            features['objective_distance'] = min_distance
-        # Controlling passive behaviour based on if the agent is on the offense or defense
-        if features['on_defense'] == 1:
-            if action == Directions.STOP:
-                features['stop'] = 0
-        elif features['on_defense'] == 0:
-            if action == Directions.STOP:
-                features['stop'] = 1
+            features['objective_distance'] = min_my_food_distance
         return features
 
     def get_weights(self, game_state, action):
